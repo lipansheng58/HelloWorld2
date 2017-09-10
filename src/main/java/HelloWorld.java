6d4d35e63311e692e95582e0327d121d3cc59ec2
panpan <lipansheng58@163.com>
2017-09-10 19:54:33 +0800

    add master

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/9/10 0010.
+ */
+public class HelloWorld {
+
+    static public  void  main( String args [] )
+    {
+        System.out.println("Hello world!");
+    }
+}
